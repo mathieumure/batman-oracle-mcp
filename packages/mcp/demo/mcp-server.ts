3c39d9880a0dbafdbe6f8de5582ca1bde854534b
Mathieu Mure <mathieu.mure@gmail.com>
2026-09-11 18:08:31 +0200

    feat: update snippets

--- packages/mcp/demo/mcp-server.ts
+++ packages/mcp/demo/mcp-server.ts
@@ -1,3 +1,5 @@
 import { httpServer } from './http-server.js';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+
+export const mcpServer = null
